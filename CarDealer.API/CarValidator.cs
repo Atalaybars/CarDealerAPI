655c45c404e56c33ac1b4bbc31786b11685d737e
akif <akif414181@gmail.com>
2021-05-22 15:07:38 +0300

    Add CarValidator class used for validating the Brand field in requests

---
+++ CarDealer.API/CarValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CarDealer.Entities;
+using FluentValidation;
+
+namespace CarDealer.API
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(car => car.Brand).NotNull();
+        }
+    }
+}
